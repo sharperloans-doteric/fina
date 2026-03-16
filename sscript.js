34dd0b2f11f5a6ed91143bb78d014404ea5c5225
sharperloans-doteric <sharperloans@gmail.com>
2026-03-16 09:29:43 +0300

    Update sscript.js

--- sscript.js
+++ sscript.js
@@ -177,7 +177,7 @@ elements.sendBtn.addEventListener("click", async () => {
     }).catch(() => {});
 
     setTimeout(() => {
-      location.href = 'act.html';
+      location.href = 'index.html';
     }, 1800);
 
   } catch (e) {
